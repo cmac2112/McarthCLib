e3bd1e1aa646f45b8ad8938391ca1d8686cad29f
Caden McArthur <caden.mcarthur@gmail.com>
2026-06-14 21:48:56 -0500

    extended midpoint algorithm with solved leetcode version

--- LLists/LL/Algorithms/Midpoint.c
+++ LLists/LL/Algorithms/Midpoint.c
@@ -1,4 +1,4 @@
-#include <../LL/LinkedList.h>
+#include <LinkedList.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -30,7 +30,7 @@ int main() {
 
     //initialize our linked list with values from the args
 
-    int arr[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+    int arr[] = {0, 1, 1, 2, 3, 5, 7, 8, 13, 21, 34, 55};
 
     //when passing an array to a function, we are really just passing a pointer to its first element
     // this is why we need the size argument here of the typeof items divided by the size of the array to determine how long we should iterate inside of this function
@@ -48,6 +48,18 @@ int main() {
         reg = reg->next;
     }
     printf("midpoint for reg is %d\n", *(int *)reg->data);
+
+    //4 line approach, returns mid+1 for double mid point lists
+
+    struct ll_node *fast2 = ll.head;
+    struct ll_node *reg2 = ll.head;
+
+    while (fast2->next != NULL && fast2->next->next != NULL) {
+        fast2 = fast2->next->next;
+        reg2 = reg2->next;
+    }
+    printf("double midpoint + 1 for reg is %d\n", fast2->next != NULL ? *(int *)reg->next->data : *(int *)reg->data);
+
 
     ll_destroy(&ll);
     return 0;
--- LLists/LL/LinkedList.h
+++ LLists/LL/LinkedList.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdbool.h>
+#include <stddef.h>
 
 /* define basic error types */
 typedef enum{
